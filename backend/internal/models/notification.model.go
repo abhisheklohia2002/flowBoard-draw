515fff42d783ef6daea9bb286de76c658d79090a
Abhishek lohia <abhisheklohia@tripxl.com>
2026-07-04 21:42:22 +0530

    add change in notification model

--- backend/internal/models/notification.model.go
+++ backend/internal/models/notification.model.go
@@ -8,18 +8,16 @@ type Notification struct {
 	UserID uint `json:"user_id" gorm:"not null;index"`
 	User   User `json:"user,omitempty" gorm:"foreignKey:UserID"`
 
-	ActorID uint `json:"actor_id" gorm:"not null;index"`
-	Actor   User `json:"actor,omitempty" gorm:"foreignKey:ActorID"`
-
-	DiagramID uint    `json:"diagram_id" gorm:"index"`
-	Diagram   Diagram `json:"diagram,omitempty" gorm:"foreignKey:DiagramID"`
-
-	Type string `json:"type" gorm:"not null"` // collaboration_invite, collaborator_added, diagram_updated
+	Type string `json:"type" gorm:"not null"`
+	// collaboration_invite, collaboration_removed, role_updated
 
 	Title   string `json:"title" gorm:"not null"`
 	Message string `json:"message" gorm:"not null"`
 
-	ReadAt *time.Time `json:"read_at"`
+	DiagramID *uint `json:"diagram_id,omitempty" gorm:"index"`
+	ProjectID *uint `json:"project_id,omitempty" gorm:"index"`
+
+	IsRead bool `json:"is_read" gorm:"not null;default:false"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
