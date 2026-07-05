9842ab1d9351b30520c65316f0a6e1fba80a6ad3
Abhishek lohia <abhisheklohia@tripxl.com>
2026-07-05 22:25:26 +0530

    add laxmode

--- backend/internal/helpers/cookies.go
+++ backend/internal/helpers/cookies.go
@@ -15,9 +15,9 @@ func SetAuthCookies(c *gin.Context, accessToken string, refreshToken string) {
 	isProduction := os.Getenv("APP_ENV") == envFile
 
 	sameSite := http.SameSiteLaxMode
-	if isProduction {
-		sameSite = http.SameSiteNoneMode
-	}
+	// if isProduction {
+	// 	sameSite = http.SameSiteNoneMode
+	// }
 
 	http.SetCookie(c.Writer, &http.Cookie{
 		Name:     "access_token",
